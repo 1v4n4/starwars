836fa4d6840e0d88d0144ba1f8600414bb8370e9
1v4n4 <ivananl@yahoo.com>
2022-04-11 22:29:25 +0200

    Add fetching planets

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Planet from './Planet';
+
+const fetchPlanets = async () => {
+  const res = await fetch('https://swapi.dev/api/planets/');
+  return res.json();
+};
 
 const Planets = () => {
+  const { data, status } = useQuery('planets', fetchPlanets);
   return (
     <div>
       <h1>Planets</h1>
+      {status === 'loading' && <p>Loading...</p>}
+      {status === 'error' && <p>Error!</p>}
+      {status === 'success' && (<div>
+        {data.results.map(planet => <Planet planet={planet} />)}
+        </div>)}
     </div>
   )
 }
