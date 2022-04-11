2a216b810031d16e2a1b7c14597907769081838a
1v4n4 <ivananl@yahoo.com>
2022-04-11 21:29:20 +0200

    Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+    <button>Planets</button>
+      <button>People</button>
+    </div>
+  )
+}
+
+export default Navbar;
