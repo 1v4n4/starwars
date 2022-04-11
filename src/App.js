923330debcf3305ab4129c906baab49f8938f795
1v4n4 <ivananl@yahoo.com>
2022-04-11 21:29:01 +0200

    Add app setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import Navbar from './components/Navbar';
+import './CSS/App.css';
+import Planets from './components/Planets';
+import People from './components/People';
+
+const App = ()  => {
+  const [page, setPage] = useState('people');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+        <div className="content">
+          { page === 'people' ? <People /> : <Planets />}
+        </div>
     </div>
   );
 }
