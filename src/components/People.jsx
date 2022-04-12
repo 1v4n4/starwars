ddaf80e06d61e54346b150f18d5506b8477b8316
1v4n4 <ivananl@yahoo.com>
2022-04-12 12:04:44 +0200

    Add people fetching and displaying

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import Person from './Person';
 
-const fetchPeople = async () => {
-  const res = await fetch('https://swapi.dev/api/people/');
+const fetchPeople = async (page) => {
+  const res = await fetch(`https://swapi.dev/api/people/?page=${page}`);
   return res.json();
 };
 
 const People = () => {
-  const { data, status } = useQuery('people', fetchPeople);
+    const [page, setPage] = useState(1);
+    const { data, status } = useQuery(['people', page ], () => fetchPeople(page), {
+    keepPreviousData: true,
+  })
   console.log(data);
   return (
     <div>
       <h1>People</h1>
       {status === 'loading' && <p>Loading...</p>}
       {status === 'error' && <p>Error!</p>}
+      <button onClick={() => setPage(old => old-1 > 0 ? old -1 : old)} disabled={page === 1}>Previous</button>
+      {page}
+      <button onClick={() => setPage(old => data.next ? old + 1 : old)} disabled={!data || !data.next}>Next</button>
       {status === 'success' && (<div>{data.results.map(person => <Person person={person} key={person.name}/>)}</div>)}
     </div>
   )
